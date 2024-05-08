fd89b1782c08ecbece428b3b4e90f71c56cc0b1c
princedevelop <princesuhagiya94@gmail.com>
2024-05-08 13:30:59 +0530

    simple data.json view in app file and create filter ccomponent with simple ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,38 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import FilterComponent from "./components/Filter.jsx";
 import { Data } from "./data";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <FilterComponent />
+      <h2>Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>City</th>
+            <th>Category</th>
+            <th>Type</th>
+            <th>Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.city}</td>
+              <td>{item.category}</td>
+              <td>{item.type}</td>
+              <td>{item.active}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
