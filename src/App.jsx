9ce76e73ba8b76a4a807f8a1eeb69ab9e49c4aa8
princedevelop <princesuhagiya94@gmail.com>
2024-05-08 16:01:03 +0530

    complated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import { Data } from "./data";
 
